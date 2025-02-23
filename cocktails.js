57a6c8b4ea8a1c97e52985c985cfb8e9da566b99
Victor Furustubbe <mantykanto2@gmail.com>
2025-02-23 02:16:28 +0100

    node assignment commits

--- cocktails.js
+++ cocktails.js
@@ -1,45 +1,100 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
-const PORT = '3000';
+const PORT = '3001';
 
-http.createServer((req,res) => {
-    const fullpath = url.parse(req.url, true)
+http.createServer((req, res) => {
+    const fullpath = url.parse(req.url, true);
 
-    console.log('href :' + fullpath.href) // /manhattan?whiskey=bourbon
-    console.log('path :' + fullpath.pathname)  // /manhattan
-    console.log('search :' + fullpath.search) // ?whiskey=bourbon
-    
-    res.writeHead(200, 'Whoohoo connection is successful!', {'Content-type': 'text/html'}) //now we can send html back to the client.
-    
+    // console.log(fullpath)
 
-    // if (req.url === '/') {
-    //     res.write('<h1>Welcome to the Cocktail page!</h1>')
-    //     res.write('<p>This is a page w. some History and Ingredients about The old Fashioned and Manhattan!</p>')
-    //     res.write('<a href="/manhattan">Manhattan</a>')
-    //     res.end()
-    // }
+    // console.log('href: ' + fullpath.href) // /manhattan?rye=ingredients
+    // console.log('pathname: ' + fullpath.pathname) // /manhattan
+    // console.log('search:' + fullpath.search) // ?rye=ingredients
 
+    let queries = fullpath.query;
+    // console.log('bourbon: ' + queries.bourbon); //bourbon=ingredients
+
+    res.writeHead(200, 'Connection successful', {'Content-type': 'text/html'})
     if (req.url === '/') {
-        res.write('<h1>Welcome to the Cocktail page!</h1>')
-        res.write('<p>This is a page with some History and Ingredients about The Old Fashioned and Manhattan!</p>')
-        res.write('<a href="/manhattan">Manhattan</a>')
-        res.write('<a href="/oldfashioned">The Old Fashioned</a>')
-        res.end()
-    } else if (req.url === '/manhattan') {
-        res.write('<h1>Manhattan Cocktail</h1>')
-        res.write('<p>History and Ingredients of the Manhattan Cocktail.</p>')
-        res.write('<p>Whiskey: Bourbon or Rye, Vermouth: Sweet, Garnish: Cherry.</p>')
-        res.end()
-    } else if (req.url === '/oldfashioned') {
-        res.write('<h1>The Old Fashioned Cocktail</h1>')
-        res.write('<p>History and Ingredients of the Manhattan Cocktail.</p>')
-        res.write('<p>Whiskey: Bourbon or Rye, Vermouth: Sweet, Garnish: Cherry.</p>')
-        res.end()
+        res.write('<h1>Welcome to the Cocktails page!</h1>')
+        res.write("<a style='margin-right: 16px' href='/manhattan'>Manhattan</a>")
+        res.write("<a style='margin-right: 16px' href='/oldfashioned'>The Old Fashioned</a>")
+    }
+    
 
-    } else {
-        res.write('<h1>404: Page not found</h1>');
-        res.end();
+    if(fullpath.pathname === '/oldfashioned') {
+        res.write('<h1>Welcome to The Old Fashioned page</h1>')
+        res.write("<a style='margin-right: 16px' href='/'>Home</a>")
+        res.write("<a style='margin-right: 16px' href='/manhattan'>Manhattan</a>")
+        res.write('<div><p>Here is the history and the ingredients:</p></div>')
+        res.write("<a style='margin-right: 16px' href='/oldfashioned?bourbon=history'>History</a>")
+        res.write("<a style='margin-right: 16px' href='/oldfashioned?bourbon=ingredients'>Ingredients</a>")
+
+        if (queries.bourbon === 'history') {
+            fs.readFile('./data/ofhistory.html', (error, data) => {
+                if (error) {
+                    res.write('Something went wrong')                    
+                } else {
+                    res.write(data)
+                }
+                res.end();
+
+            });
+            
+        }
+
+        if (queries.bourbon === 'ingredients') {
+            fs.readFile('./data/ofingredients.html', (error, data) => {
+                if (error) {
+                    res.write(`<div>Something went wrong, there's an ${error}</div>`)
+                } else {
+                    res.write(data)
+                }
+                res.end();
+            });
+            
+        }
     }
 
-}).listen(PORT, () => console.log(`listening to port ${PORT}`));
+    if(fullpath.pathname === '/manhattan') {
+        res.write('<h1>Welcome to The Manhattan page</h1>')
+        res.write("<a style='margin-right: 16px' href='/'>Home</a>")
+        res.write("<a style='margin-right: 16px' href='/oldfashioned'>The Old Fashioned</a>")
+        res.write('<div><p>Here is the history and the ingredients:</p></div>')
+        res.write("<a style='margin-right: 16px' href='/manhattan?rye=history'>History</a>")
+        res.write("<a style='margin-right: 16px' href='/manhattan?rye=ingredients'>Ingredients</a>")
+
+        if (queries.rye === 'history') {
+            fs.readFile('./data/manhistory.html', (error, data) => {
+                if (error) {
+                    res.write('Something went wrong')                    
+                } else {
+                    res.write(data)
+                }
+                res.end();
+
+            });
+            
+        }
+
+        if (queries.rye === 'ingredients') {
+            fs.readFile('./data/maningredients.html', (error, data) => {
+                if (error) {
+                    res.write(`<div>Something went wrong, there's an ${error}</div>`)
+                } else {
+                    res.write(data)
+                }
+                res.end();
+            });
+            
+        }
+    }
+    
+    res.end();
+
+
+
+
+    
+}).listen(PORT, () => console.log(`Listening to port: ${PORT}`))
